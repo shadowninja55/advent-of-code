95281e1b711ff119df51f51e1434b381c71ff6ce
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-04 19:14:46 -0500

    Update day1.nim

--- 1/day1.nim
+++ 1/day1.nim
@@ -2,7 +2,7 @@ import strutils
 import sets
 import sequtils
 
-func solvePartOne(nums: seq[int], target: int): int =
+proc solvePartOne(nums: seq[int], target: int): int =
     var seen = initHashSet[int]()
 
     for num in nums:
@@ -11,16 +11,13 @@ func solvePartOne(nums: seq[int], target: int): int =
         
         seen.incl(num)
 
-func solvePartTwo(nums: seq[int], target: int): int =
+proc solvePartTwo(nums: seq[int], target: int): int =
     for i in nums:
         let j = solvePartOne(nums, target - i)
 
         if j > 0: 
             return i * j
 
-proc main() =
-    let nums = toSeq(lines("input.txt")).map(parseInt)
-    echo solvePartOne(nums, 2020)
-    echo solvePartTwo(nums, 2020)
-
-main()
+let nums = toSeq(lines("input.txt")).map(parseInt)
+echo solvePartOne(nums, 2020)
+echo solvePartTwo(nums, 2020)
