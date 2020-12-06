7536e9a7c2c9aa8a92ee8fa4eeed40e3a1ca1b8f
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-06 11:03:09 -0500

    Update day6.nim

--- day6.nim
+++ day6.nim
@@ -1,17 +1,19 @@
 import sequtils
-import sets
 import strutils
+
+func toCharSet[T](s: T): set[char] =
+    for c in s: result.incl(c)
 
 func solvePartOne(groups: seq[string]): int =
     for group in groups:
-        result += group.filter(isLowerAscii).toHashSet().len
+        result += group.filter(isLowerAscii).toCharSet().len
 
 func solvePartTwo(groups: seq[string]): int =
     for group in groups:
-        var answered = toSeq('a'..'z').toHashSet()
+        var answered = {'a'..'z'}
 
         for person in group.splitLines():
-            answered = answered * person.toHashSet()
+            answered = answered * person.toCharSet()
         
         result += answered.len
 
