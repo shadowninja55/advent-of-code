22580aba975a77acc17153bee23ca06712d7a47d
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-13 23:18:44 -0500

    cleaned up day 3

--- 03/day3.nim
+++ 03/day3.nim
@@ -10,12 +10,10 @@ proc solvePartOne(deltaX, deltaY: int): int =
         if grid[y][x] == '#':
             inc result
 
-        x += deltaX
-        while x >= width:
-            x -= width
+        x = (x + deltaX) mod width
 
 proc solvePartTwo(deltas: varargs[(int, int)]): int =
-    prod(deltas.map(d => solvePartOne(d[0], d[1])))
+    prod deltas.map(d => solvePartOne(d[0], d[1]))
 
 echo solvePartOne(3, 1)
 echo solvePartTwo((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
