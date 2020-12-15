4b5ee88e12634791fa636795ecab1678ce90d3fd
shadowninja55 <shadowninja1050@gmail.com>
2020-12-14 20:58:10 -0500

    cleaned up day 10

--- 10/day10.nim
+++ 10/day10.nim
@@ -1,26 +1,22 @@
-import algorithm, intsets, sequtils, strutils, tables
+import algorithm, sequtils, strutils, tables
 
 proc solvePartOne(adapters: seq[int]): int =
     var
-        nums = {1: 0, 3: 0}.toTable
+        nums: CountTable[int]
         last = adapters[0]
 
     for adapter in adapters[1..^1]:
-        let diff = adapter - last
-        inc nums[diff]
+        nums.inc(adapter - last)
         last = adapter
 
     nums[1] * nums[3]
 
 proc solvePartTwo(adapters: seq[int]): int =
-    var poss = {0: 1}.toTable
+    var poss = newCountTable [0]
 
     for adapter in adapters[1..^1]:
         for i in 1..3:
-            let diff = adapter - i
-
-            if diff in poss:
-                poss.mgetOrPut(adapter, 0) += poss[diff]
+            poss.inc(adapter, poss[adapter - i])
 
     poss[adapters.max]
 
