fcbfd7e3e9716094872b6dbc53cd5da4bf07877f
shadowninja55 <shadowninja1050@gmail.com>
2020-12-18 11:01:48 -0500

    made day 18 more cursed

--- 18/day18.nim
+++ 18/day18.nim
@@ -1,46 +1,17 @@
-import macros, strutils, os, osproc
+import macros, math, strutils
 
-const code = """
 proc `>+<`(a, b: uint64): uint64 =
   a + b
 
 proc `>*<`(a, b: uint64): uint64 =
   a * b
 
-var result: uint64 = 0
+macro solvePartOne(input: static[string]): uint64 =
+  parseExpr(input.multiReplace(("+", ">+<"), ("*", ">*<")))
 
-"""
+macro solvePartTwo(input: static[string]): uint64 = 
+  parseExpr(input.replace("*", ">*<"))
 
-proc process(replacements: varargs[(string, string)]): uint64 =
-  # writing to file
-  var file = open("eval.nim", fmWrite)
-  file.write code
-
-  for line in lines "input.txt":
-    var replaced = line
-
-    for replacement in replacements:
-      replaced = replaced.replace(replacement[0], replacement[1])
-
-    let fixed = "result += " & replaced & "\n"
-    file.write fixed
-  
-  file.write "\necho result"
-  file.close
-
-  # compiling
-  discard execShellCmd "nim c eval.nim > nul"
-  result = parseUInt(execProcess("eval").strip)
-  
-  # cleaning up
-  removeFile "eval.nim"
-  removeFile "eval.exe"
-
-proc solvePartOne: uint64 =
-  process(("+", ">+<"), ("*", ">*<"))
-
-proc solvePartTwo: uint64 = 
-  process(("*", ">*<"))
-
-echo solvePartOne()
-echo solvePartTwo()
+const input = "sum [" & slurp("input.txt").replace("\n", ",") & "]"
+echo solvePartOne input
+echo solvePartTwo input
