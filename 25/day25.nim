4181440578ad278516c638b8db6f5c1e0aa18a8e
shadowninja55 <shadowninja1050@gmail.com>
2020-12-25 11:42:32 -0500

    cleaned up day 25

--- 25/day25.nim
+++ 25/day25.nim
@@ -2,20 +2,16 @@ const
   card = 14205034
   door = 18047856
 
-func calcLoops(key: int): int =
-  var value = 1
-  
-  for loops in 1..int.high:
-    value = (value * 7) mod 20201227
-
-    if value == key:
-      return loops
-
 func solvePartOne: int =
+  var value = 1
   result = 1
 
-  for _ in 1..door.calcLoops():
-    result = (result * card) mod 20201227
+  for loops in 1..int.high:
+    value = value * 7 mod 20201227
+    result = result * card mod 20201227
+
+    if value == door:
+      return
 
 func solvePartTwo: string =
   "go finish the rest of your puzzles, you've got this!"
